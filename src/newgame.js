8d1788260a872a6dbbc4c6118064096487048193
Johnni Mortensen <jm@lscontrol.dk>
2019-06-13 14:33:25 +0200

    Adds logic in newgame

--- src/newgame.js
+++ src/newgame.js
@@ -1,28 +1,90 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
-import AddPlayer from "./addplayer";
+import Modal from "./modal";
 import { ButtonControl, LinkButton, Button } from "./buttons";
 const Container = styled.section`
     display: grid;
 `;
+const InputControl = styled.input`
+    border: 0;
+    padding: 5px 10px;
+    margin: 15px 0px;
+    background-color: #228343;
+    color: white;
+    font-size: 1rem;
+`;
 
+const PlayerList = styled.ul`
+    list-style: none;
+    margin: 25px auto;
+    padding: 0;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    max-width: 300px;
+    align-self: center;
+`;
+
+const PlayerListItem = styled.li`
+    position: relative;
+    font-size: 1.2rem;
+    width: 100%;
+    text-align: center;
+    margin-bottom: 10px;
+    padding: 0 15px;
+    &::after {
+        content: "";
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        position: absolute;
+        height: 1px;
+        background-color: transparent;
+        box-shadow: 1px 0 3px 0 white;
+    }
+`;
 const NewGame = () => {
     const [isAddingPlayer, setIsAddingPlayer] = useState(false);
+    const [newPlayer, setNewPlayer] = useState("");
+    const [players, setPlayers] = useState(["Johnni", "Andreas"]);
 
     return (
         <Container>
-            <AddPlayer
-                visable={isAddingPlayer}
-                done={() => setIsAddingPlayer(false)}
-            />
-            <div>New Game!</div>
+            <Modal visable={isAddingPlayer}>
+                <InputControl
+                    value={newPlayer}
+                    onChange={e => setNewPlayer(e.target.value)}
+                />
+                <ButtonControl>
+                    <Button
+                        onClick={() => {
+                            if (newPlayer !== "") {
+                                setPlayers([...players, newPlayer]);
+                                setNewPlayer("");
+                                setIsAddingPlayer(false);
+                            }
+                        }}
+                    >
+                        Add
+                    </Button>
+                    <Button onClick={() => setIsAddingPlayer(false)}>
+                        Cancel
+                    </Button>
+                </ButtonControl>
+            </Modal>
             <ButtonControl>
                 <Button onClick={() => setIsAddingPlayer(true)}>
                     Add Player
                 </Button>
                 <LinkButton to="/game">Start Game</LinkButton>
             </ButtonControl>
+            <PlayerList>
+                {players.map(player => (
+                    <PlayerListItem key={player}>{player}</PlayerListItem>
+                ))}
+            </PlayerList>
         </Container>
     );
 };
